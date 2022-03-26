8575dd052b7894a230732e76e19f64a323f879a7
Ahmet Seker <ahmetsheker09gmail.com>
2022-03-26 22:57:09 +0300

    4th commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,6 +2,8 @@ public class FirstJavaClass {
 
     public static void main(String[] args) {
 
+        System.out.println("\n>==============<\n");
+
         System.out.println("Learning github");
 
         System.out.println("\n>==============<\n");
